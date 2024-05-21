fed373634cc9bd6bb51a837a3e0a7af8e425550d
Marel Agonglo <agonglomarel@mail.com>
2024-05-21 13:44:02 +0100

    Java

--- backend/src/App.java
+++ backend/src/App.java
@@ -1,5 +1,11 @@
+import database.PolyBayDatabase;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        PolyBayDatabase poly1 = new PolyBayDatabase("localhost", 3306, "poly_bay", "Marel","M@relsig@n152");
+        
     }
+    
 }
---
+++ backend/src/database/PolyBayDatabase.java
@@ -0,0 +1,13 @@
+package database;
+
+import java.sql.SQLException;
+
+public class PolyBayDatabase extends MySQLDatabase {
+
+    public PolyBayDatabase(String host, int port, String databaseName, String user, String password)
+            throws SQLException {
+        super(host, port, databaseName, user, password);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
